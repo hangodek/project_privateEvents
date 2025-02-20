1c0ad0e7842f50ff5215edf22465e03dc763e834
han <icyfarhan@gmail.com>
2025-02-20 17:05:55 +0700

    Completed the new event logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,14 +9,29 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.hoster = Current.user
+
+    if @event.save
+      redirect_to events_path
+    else
+      redirect_to events_path
+    end
   end
 
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
     redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :name, :description, :location ])
   end
 end
