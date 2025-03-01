5fba1412e2c3f7d0bf80440df065797091990907
han <icyfarhan@gmail.com>
2025-03-01 10:31:24 -0500

    Completed the homepages available events section

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,5 +2,9 @@ class HomepagesController < ApplicationController
   allow_unauthenticated_access only: %i[ index ]
 
   def index
+    if authenticated?
+      @user = Current.user.email_address
+      @events = Event.all
+    end
   end
 end
